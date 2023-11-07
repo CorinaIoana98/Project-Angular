c5f07d5fdb1e76004e08e44449cc18ecb0231dc8
burtanadrian <114177492+burtanadrian@users.noreply.github.com>
2023-11-07 12:03:37 +0200

    07.11
    07.11

--- src/app/menu/myshifts/myshifts.component.ts
+++ src/app/menu/myshifts/myshifts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth-user.service';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-myshifts',
